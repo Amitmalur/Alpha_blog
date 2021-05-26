ba77e7e868535e2d3d316050128825c6b6c24ead
Amit Malur <amitmalur@gmail.com>
2021-05-26 09:36:15 +0530

    Added show, edit, update, create functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,48 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def index
+        @articles= Article.all
+
+    end
+
+    def new
+        @article = Article.new
+            
+    end
+
+    
+    def edit
+        @article = Article.find(params[:id])
+    
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article has been saved successfully"
+            redirect_to @article
+        else
+            render 'new'
+        
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article has been updated successfully"
+            redirect_to @article
+        
+        else
+            render 'edit'
+
+        
+        end
+
+    end
+    
+
 
 
 
